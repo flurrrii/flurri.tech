129b5692db5ffe345a9b7d8c9ede29c5ff8968b0
flurri <94768115+flurrrii@users.noreply.github.com>
2022-10-29 09:45:04 -0500

    stuff

---
+++ script.js
@@ -0,0 +1,55 @@
+const content = document.getElementById('content').querySelector('span');
+let current = "";
+const text = `Hey, I'm flurri!`;
+let index = 0;
+
+const loop = setInterval(() => {
+    current += text[index];
+    content.innerText = current;
+    index++;
+    if(index >= text.length) {
+        clearInterval(loop);
+    }
+}, 50);
+
+
+//Resize
+resize = () => {
+    if(window.innerWidth < 1104){
+        //too lazy to make it responsive
+        document.body.style.display = 'none';
+    }else{
+        document.body.style.display = 'block';
+    }
+}
+window.addEventListener('resize', resize);
+
+const textEditContent = document.getElementById('content');
+const windows = document.getElementById('windows');
+const tabs = document.getElementById('tabs');
+
+let pageContent = false;
+
+updatePage = () => {
+    const hash = window.location.hash.slice(1);
+    if(hash){
+        if(pageContent)pageContent.setAttribute('hidden', true);
+        textEditContent.style.right = '100%';
+        windows.style.right = '100%';
+        tabs.style.left = '10%';
+        pageContent = document.getElementById(hash);
+        pageContent.removeAttribute('hidden');
+        document.title = `flurri | ${hash}`
+    }else{
+        if(pageContent)
+        pageContent.setAttribute('hidden', true);
+        textEditContent.style.left = '810px;';
+        windows.style.right = '40%';
+        tabs.style.left = '67%';
+        document.title = 'flurri'
+    }
+}
+
+updatePage();
+
+window.addEventListener('hashchange', updatePage);
