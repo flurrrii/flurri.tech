0001e5e39ed42da51f52526eeee6c207952fe7ea
flurri <94768115+flurrrii@users.noreply.github.com>
2022-11-05 09:50:13 -0500

    projects page update

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+
+const phrases = ["Hey, I'm flurri!", "abooby is smarter", "photop.live", "never gonna give you up.", "🤨"];
 const content = document.getElementById('content').querySelector('span');
-let current = "";
-const text = `Hey, I'm flurri!`;
-let index = 0;
+function randomPhrase() {
+    content.innerText = "";
+    let current = "";
+    const text = phrases[Math.floor(Math.random() * phrases.length)];;
+    let index = 0;
 
-const loop = setInterval(() => {
-    current += text[index];
-    content.innerText = current;
-    index++;
-    if(index >= text.length) {
-        clearInterval(loop);
-    }
-}, 50);
-
+    const loop = setInterval(() => {
+        current += text[index];
+        content.innerText = current;
+        index++;
+        if (index >= text.length) {
+            clearInterval(loop);
+        }
+    }, 50);
+}
 
 //Resize
 resize = () => {
-    if(window.innerWidth < 1104){
+    if (window.innerWidth < 1104) {
         //too lazy to make it responsive
         document.body.style.display = 'none';
-    }else{
+    } else {
         document.body.style.display = 'block';
     }
 }
@@ -32,21 +36,22 @@ let pageContent = false;
 
 updatePage = () => {
     const hash = window.location.hash.slice(1);
-    if(hash){
-        if(pageContent)pageContent.setAttribute('hidden', true);
+    if (hash) {
+        if (pageContent) pageContent.setAttribute('hidden', true);
         textEditContent.style.right = '100%';
         windows.style.right = '100%';
         tabs.style.left = '10%';
         pageContent = document.getElementById(hash);
         pageContent.removeAttribute('hidden');
         document.title = `flurri | ${hash}`
-    }else{
-        if(pageContent)
-        pageContent.setAttribute('hidden', true);
+    } else {
+        if (pageContent)
+            pageContent.setAttribute('hidden', true);
         textEditContent.style.left = '810px;';
         windows.style.right = '40%';
         tabs.style.left = '67%';
         document.title = 'flurri'
+        randomPhrase();
     }
 }
 
